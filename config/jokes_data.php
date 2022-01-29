259b545381939dbca442dc661a8d7e7ea06acae5
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-29 22:56:45 +0100

    aggiunto file di raccolta barzellette nella cartella config

---
+++ config/jokes_data.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    [
+        'title' => 'Sfortunato',
+        'description' => 'Un uomo  infelicemente sposato torna a casa completamente ubriaco. Suona e gli apre sua moglie. Lui la guarda ed esclama:
+        - Io sono veramente sfortunato.
+        - Perché?
+        - Tutta la sera ho bevuto per dimenticarti e adesso ti vedo doppia.',
+        'ratings' => 6,
+    ],
+    [
+        'title' => 'Cibo',
+        'description' => 'Una donna grassa dice a suo marito molto magro:
+        - Se uno ti guarda, potrebbe pensare che il mondo stia morendo di fame.
+        Marito risponde:
+        - E se guarda te, potrebbe pensare che sia colpa tua.',
+        'ratings' => 8,
+    ],
+    [
+        'title' => 'Siamo a Napoli',
+        'description' => 'Da qualche parte a Napoli, un poliziotto ferma 3 persone su una moto e dice:
+        - Ma siete normali? Tre persone su una moto.
+        Uno dei passeggeri si gira e dice:
+        - Cavolo, Pasquale è caduto.',
+        'ratings' => 7,
+    ],
+    [
+        'title' => 'Maleducato',
+        'description' => 'Durante un incontro di pugilato, uno dei pugili subisce una botta forte e finisce sul tappeto. Mentre l\'arbitro gli sta contando, dal pubblico urla una nonnina:
+        - Ma guarda, mica si alza questo; io lo conosco dall\'autobus.',
+        'ratings' => 6,
+    ],
+    [
+        'title' => 'Vita insieme',
+        'description' => 'Mentre marito e moglie fanno amore, a lui sembra di sentire l\'odore di fumo. Si rivolge alla moglie:
+        - Tu non senti niente?
+        - No caro, da anni!',
+        'ratings' => 7,
+    ],
+    [
+        'title' => 'Topi studiosi',
+        'description' => 'Tra mamme topo:
+        - Sai mio figlio quest\'anno si è iscritto all\'università!
+        - Davvero! Il mio invece si è laureato proprio quest\'anno.
+        - Ah si, e in cosa?
+        - Ingegneria informatica e pensa che ha già trovato lavoro!
+        - Bravo! E cosa fa?
+        - Il mouse.',
+        'ratings' => 8,
+    ],
+];
---
+++ database/migrations/2022_01_29_213607_create_jokes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJokesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jokes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 60);
+            $table->string('slug');
+            $table->text('description', 30);
+            $table->tinyInteger('ratings')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jokes');
+    }
+}
