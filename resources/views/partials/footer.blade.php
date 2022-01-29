0456b3516291504fa3ad04d1c4774d6fdc30a924
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-29 20:33:11 +0100

    style for footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,8 @@
-<footer>Sono un footer</footer>
+<footer class="footer">
+    <div class="footer-left">
+        &copy Smiling Jokes - <em>All rights reserved</em> 
+    </div>
+    <div class="footer-right">
+        Made with &hearts; by <a href="https://github.com/elepot87" target='_blank'>Eleonora</a>
+    </div>
+</footer>
