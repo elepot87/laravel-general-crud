211757a708e90125fefceef7b64ca96f584ddbdb
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-29 20:08:04 +0100

    style for header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,29 @@
-<header>Sono un header</header>
+<header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="{{asset('images/logo-jokes.png')}}" alt="logo" class="d-inline-block align-text-center logo">
+        Smiling <span class="highlighted">Jokes</span> 
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link @if (Request::route()->getName()==='home')
+                        active
+                    @endif" href="#"
+                    aria-current="page" href="{{route('home')}}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Create Jokes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacts</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+</header>
