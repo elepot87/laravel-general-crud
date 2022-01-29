d9c6d79b697e370e1546cd4493334083429488d0
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-29 23:59:38 +0100

    Controller resource

---
+++ resources/views/jokes/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('head-title', 'Jokes | Smiling Jokes')
+
+@section('main-content')
+<main>
+    <div class="container">
+        <h1 class="animate__animated animate__fadeInDown">Pagina archivio Jokes</h1>
+    </div>
+</main>
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="{{route('home')}}">
             <img src="{{asset('images/logo-jokes.png')}}" alt="logo" class="d-inline-block align-text-center logo">
         Smiling <span class="highlighted">Jokes</span> 
         </a>
@@ -11,10 +11,10 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link @if (Request::route()->getName()==='home')
+                    <a class="nav-link @if (Request::route()->getName()==='jokes.index')
                         active
-                    @endif" href="#"
-                    aria-current="page" href="{{route('home')}}">Home</a>
+                    @endif" href="{{route('jokes.index')}}"
+                    aria-current="page" href="{{route('jokes.index')}}">Jokes</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Create Jokes</a>
